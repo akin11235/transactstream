0072a61a9436175e1cb40c888108db21a3878239
akin11235 <akin11235@gmail.com>
2025-09-22 16:41:54 -0400

    Format terraform local files

--- infra/terraform/modules/adls_container/main.tf
+++ infra/terraform/modules/adls_container/main.tf
@@ -1,5 +1,5 @@
 resource "azurerm_storage_container" "this" {
   name                  = var.container_name
-  storage_account_id    = var.storage_account_id   # updated
+  storage_account_id    = var.storage_account_id # updated
   container_access_type = var.access_type
 }
